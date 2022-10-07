d1962a12a7620c4607cae3fb135c828e6c5e21f6
gizmore <gizmore@wechall.net>
2022-10-07 07:01:44 +0200

    '7.0.1-r1649 German Lang Pack - I'

---
+++ lang/double_accounts_de.php
@@ -0,0 +1,5 @@
+<?php
+namespace GDO\DoubleAccounts\lang;
+return [
+'module_doubleaccounts' => 'Doppelte Konten','privacy_info_double_accounts' => 'Um doppelte Konten zu erkennen, erfassen wir alle von Ihnen verwendeten IP-Adressen. Prüfen Sie, wie lange wir die IPs in der Datenbank aufbewahren.',
+'cfg_double_account_duration' => 'Aufzeichnungsdauer','tt_cfg_double_account_duration' => 'Wie lange speichern wir Ihre IP-Adressen zur Erkennung doppelter Konten?',];
